9bea16e22a76aa47e95c4e6887f5c8cfc4e6695d
harminder <kaurharminder166@gmail.com>
2019-02-05 15:19:37 +0530

    4th commit

---
+++ cls4.py
@@ -0,0 +1,15 @@
+class A:
+ def a1(self):
+     print("Hello")
+class B(A):
+ def b1(self):
+     print("I am harminder")
+class C(A):
+ def c1(self):
+     print("HW r uh")
+b=B()
+b.a1()
+b.b1()
+c=C()
+c.a1()
+c.c1()
